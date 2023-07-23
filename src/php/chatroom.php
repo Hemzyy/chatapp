75231bfba5c8adb6e422c894d4df4f58499af488
Hamza Krika <hamza.krika@etu.unistra.fr>
2023-07-24 00:34:03 +0200

    removed unecessary code

--- src/php/chatroom.php
+++ src/php/chatroom.php
@@ -35,103 +35,6 @@ session_start();
       </div>
     </div>
 
-    <!-- <script src="../js/script.js"></script> -->
-
-        <!-- Add this script tag at the end of your HTML body section -->
-    <script>
-    // Function to fetch new messages from the server
-    function fetchNewMessages() {
-        // Make an AJAX request to the server
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "get_new_messages.php", true); // Replace "get_new_messages.php" with your actual PHP file to fetch new messages
-        xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-            // If the request is successful, handle the response
-            console.log(xhr.responseText);
-            var response = JSON.parse(xhr.responseText);
-            // Update the chat interface with the new messages
-            //updateChatInterface(response.messages);
-            }else{
-                console.log("Error: " + xhr.status + " - " + xhr.statusText);
-            }
-        }
-        };
-        xhr.send();
-    }
-
-    // Call the fetchNewMessages function every 5 seconds (adjust the interval as needed)
-    setInterval(fetchNewMessages, 3000); // 5000 milliseconds = 5 seconds
-    </script>
-
-    <!-- Add this script tag after the previously added script tag -->
-    <!-- <script>
-    // Function to update the chat interface with new messages
-    function updateChatInterface(messages) {
-        // Get the chat container element
-        var chatContainer = document.getElementById("chat-messages"); // Replace "chat-container" with your actual chat container element's ID
-
-        // Loop through the new messages and append them to the chat container
-        for (var i = 0; i < messages.length; i++) {
-        var message = messages[i];
-        var messageElement = document.createElement("div");
-        messageElement.className = "message";
-        messageElement.innerHTML = message.content; // Assuming the message content is stored in the 'content' field of the message object
-        chatContainer.appendChild(messageElement);
-        }
-    }
-    </script> -->
-
-    <!-- Your existing script section in chatroom.php -->
-
-    <!-- <script>
-        // Function to send a message using AJAX
-        function sendMessage2(user, message) {
-            var xhr = new XMLHttpRequest();
-            var url = "insert_message.php"; // Update this if the PHP file is in a different location
-
-            // Create the data to be sent in the POST request
-            var data = new FormData();
-            data.append('user', user);
-            data.append('message', message);
-            console.log(data);
-
-            // Configure the AJAX request
-            xhr.open('POST', url, true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        // The message was sent successfully
-                        console.log(xhr.responseText);
-                        // You can update the chat interface here to show the sent message
-                        // For example, update the chat window with the newly sent message
-                    } else {
-                        // Handle any errors if the message couldn't be sent
-                        console.error('Error sending message: ' + xhr.status);
-                    }
-                }
-            };
-
-            // Send the request with the message data
-            xhr.send(data);
-        }
-
-        function handleMessageSend() {
-            var sender = document.getElementById("current_session_user").innerHTML;
-            var messageContent = document.getElementByClassName("message").innerHTML;
-            console.log(messageContent2);
-            sendMessage2(sender, messageContent2);
-        }
-
-        // Add event listeners to handle the "click" event for the "Send" button and the "keydown" event for the input field
-        document.getElementById("send-button").addEventListener("click", handleMessageSend);
-        document.getElementById("message-input").addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
-                event.preventDefault();
-                handleMessageSend();
-            }
-        });
-    </script> -->
 
     <script>
     // Function to send a message using AJAX
@@ -197,11 +100,6 @@ session_start();
         }
     });
 </script>
-
-
-
-
-
 
 
 </body>
